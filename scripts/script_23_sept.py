d919969ec74dc16a79c4bf2e7e06a55b70ba50ed
apoorv-on-git <apoorvelous@gmail.com>
2022-09-30 09:35:12 +0530

    changes

--- scripts/script_23_sept.py
+++ scripts/script_23_sept.py
@@ -9,49 +9,49 @@ import csv
 from app import db
 
 def sept_23():
-    all_publishers = Publisher.query.all()
+    # all_publishers = Publisher.query.all()
 
-    for publisher in all_publishers:
-        publisher.age1 = False
-        publisher.age2 = False
-        publisher.age3 = False
-        publisher.age4 = False
-        publisher.age5 = False
-        publisher.age6 = False
-        publisher.display = False
+    # for publisher in all_publishers:
+    #     publisher.age1 = False
+    #     publisher.age2 = False
+    #     publisher.age3 = False
+    #     publisher.age4 = False
+    #     publisher.age5 = False
+    #     publisher.age6 = False
+    #     publisher.display = False
 
-        db.session.add(publisher)
+    #     db.session.add(publisher)
 
-    db.session.commit()
+    # db.session.commit()
 
-    display_publishers = [
-        "Usborne Publishing Ltd",
-        "Oxford University Press",
-        "Walker Books Ltd",
-        "Disney Press",
-        "Scholastic",
-        "DK",
-        "Tiger Tales",
-        "Penguin Random House Children's UK",
-        "Pan Macmillan"
-    ]
+    # display_publishers = [
+    #     "Usborne Publishing Ltd",
+    #     "Oxford University Press",
+    #     "Walker Books Ltd",
+    #     "Disney Press",
+    #     "Scholastic",
+    #     "DK",
+    #     "Tiger Tales",
+    #     "Penguin Random House Children's UK",
+    #     "Pan Macmillan"
+    # ]
 
-    for name in display_publishers:
-        publisher_obj = Publisher.query.filter_by(name=name).first()
-        publisher_obj.age1 = True
-        publisher_obj.age2 = True
-        publisher_obj.age3 = True
-        publisher_obj.age4 = True
-        publisher_obj.age5 = True
-        publisher_obj.age6 = True
-        publisher_obj.display = True
+    # for name in display_publishers:
+    #     publisher_obj = Publisher.query.filter_by(name=name).first()
+    #     publisher_obj.age1 = True
+    #     publisher_obj.age2 = True
+    #     publisher_obj.age3 = True
+    #     publisher_obj.age4 = True
+    #     publisher_obj.age5 = True
+    #     publisher_obj.age6 = True
+    #     publisher_obj.display = True
 
-        db.session.add(publisher_obj)
+    #     db.session.add(publisher_obj)
     
-    db.session.commit()
+    # db.session.commit()
 
     tags = []
-    with open("scripts/data/23_sept/data.csv", mode="r") as file:
+    with open("scripts/data/23_sept/isbn.csv", mode="r") as file:
         csv_file = csv.reader(file)
         for line in csv_file:
             tags.append(line)
