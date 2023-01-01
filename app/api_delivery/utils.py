1b855c2af54126a5f9d2caac46228fa778743eaf
billafy <ytarkar138@gmail.com>
2023-01-01 23:55:57 +0530

    'deliveries-sort-arg'

--- app/api_delivery/utils.py
+++ app/api_delivery/utils.py
@@ -31,10 +31,10 @@ def sort_deliveries(d1, d2):
             d1_time, d2_time = d1['user']['delivery_time'].split('-')[0].strip(), d2['user']['delivery_time'].split('-')[0].strip()
             if d1_time[len(d1_time) - 2].lower() == d2_time[len(d2_time) - 2].lower():
                 return int(d1_time[0: len(d1_time) - 2]) - int(d2_time[0: len(d2_time) - 2])
-            elif d1_time[len(d1_time) - 2].lower() == 'am': 
-                return 1
-            else: 
+            elif d1_time[len(d1_time) - 2:].lower() == 'am': 
                 return -1
+            else: 
+                return 1
         if d1['user']['next_delivery_date'] < d2['user']['next_delivery_date']: 
             return -1
         else: 
