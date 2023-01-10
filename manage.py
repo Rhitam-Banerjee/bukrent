53ea004ec1076adcebaaf7f591f3915b4706b8ba
billafy <ytarkar138@gmail.com>
2023-01-11 00:32:16 +0530

    'delivery-interval-job'

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ from flask import current_app
 
 from seed_4 import *
 from export import *
-from seed_users import *
 from seed_books import seed_books
+from seed_users import *
 from script_order import *
 from scripts.script_25_aug import *
 from scripts.script_26_aug import *
@@ -21,42 +21,8 @@ from app.models.details import Detail
 from app.models.publishers import Publisher
 from app.models.reviews import Review
 from app.models.series import Series
-from dotenv import load_dotenv
-
-from flask_apscheduler import APScheduler
-
-from app import db
-from app.models.order import Order
-from sqlalchemy import cast, Date
-from app.models.user import User
-from datetime import date, timedelta
-
-load_dotenv()
 
 app = create_app()
-# app.app_context().push()
-
-# def complete_all_orders(): 
-#     with app.app_context(): 
-#         print('Completing All Orders')
-#         users = User.query.filter(User.next_delivery_date <= date.today() - timedelta(days=2)).all()
-#         for user in users: 
-#             order = Order.query.filter_by(user_id=user.id).filter(
-#                 cast(Order.placed_on, Date) == cast(user.next_delivery_date, Date),
-#             ).first()
-#             if order.is_completed: 
-#                 print(f'Completing Order Of {user.first_name} {user.last_name}')
-#                 user.last_delivery_date = user.next_delivery_date
-#                 user.next_delivery_date = user.next_delivery_date + timedelta(days=7)
-#                 user.delivery_order = 0
-#         db.session.commit()
-#         print('Completed All Orders')
-
-# complete_all_orders()
-# scheduler = APScheduler()
-# scheduler.add_job(func=complete_all_orders, trigger='interval', id='job', seconds=86400)
-# scheduler.start()
-
 cli = FlaskGroup(create_app=create_app)
 
 @cli.command()
@@ -81,12 +47,12 @@ def seed_users_data():
     seed_users()
 
 @cli.command()
-def seed_books_data(): 
-    seed_books()
-
-@cli.command()
 def script_create_orders():
     create_orders()
+
+@cli.command()
+def seed_books_data(): 
+    seed_books()
 
 @cli.command()
 def script_25_aug():
