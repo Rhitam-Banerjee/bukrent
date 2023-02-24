e6b2492438c70631a38984afb89c01e1bf190214
billafy <ytarkar138@gmail.com>
2023-02-24 23:01:32 +0530

    'book-multiple-images'

--- scripts/seed_new_books.py
+++ scripts/seed_new_books.py
@@ -40,61 +40,17 @@ def seed_new_books():
         '''
         book = books[i]
 
-        name = book[0]
-        price = book[3]
-        for_age = book[4]
-        grade_level = book[5]
-        lexile_measure = book[6]
-        pages = book[7]
-        language = book[8]
-        dimensions = book[9]
-        publisher = book[10]
         isbn = book[11]
-        main_image = book[12]
-        images = book[12:]
+        images = book[13:]
 
         added_book = NewBook.query.filter_by(isbn=isbn).first()
-        if added_book: 
-            if price: 
-                added_book.price = price.replace(',', '')
-            else: 
-                added_book.price = None
-            if for_age: 
-                added_book.for_age = for_age
-            else: 
-                added_book.for_age = None
-            if grade_level: 
-                added_book.grade_level = grade_level
-            else: 
-                added_book.grade_level = None
-            if lexile_measure: 
-                added_book.lexile_measure = lexile_measure
-            else: 
-                added_book.lexile_measure = None
-            if pages: 
-                added_book.pages = pages.replace(',', '')
-            else: 
-                added_book.pages = None
-            if language: 
-                added_book.language = language
-            else: 
-                added_book.language = None
-            if dimensions: 
-                added_book.dimensions = dimensions
-            else: 
-                added_book.dimensions = None
-            if publisher: 
-                added_book.publisher = publisher
-            else: 
-                added_book.publisher = None
-            # if main_image: 
-            #     added_book.image = main_image
-            # for image in images: 
-            #     if image: 
-            #         NewBookImage.create(image, added_book.id)
+        if added_book:
+            for image in images: 
+                if image: 
+                    NewBookImage.create(image, added_book.id)
 
             db.session.commit()
 
             print(f'{i + 1} / {len(books)}')
         else: 
-            print(f'Skipped book: {name}')            
+            print(f'Skipped book: {i + 1}')            
