5a5556cc419df843fd58f4e81fee13a22ec38bd8
Ubuntu <ubuntu@ip-172-31-3-223.ap-south-1.compute.internal>
2023-04-08 09:32:49 +0000

    shift-deliveries-fix

--- manage.py
+++ manage.py
@@ -60,16 +60,20 @@ def complete_all_orders():
 def shift_deliveries(): 
     with app.app_context(): 
         print('Shifting deliveries')
-        users = User.query.filter(User.next_delivery_date <= date.today() - timedelta(days=1)).all()
+        users = User.query.filter(User.next_delivery_date <= date.today() - timedelta(days=1)).filter_by(plan_pause_date=None, payment_status='Paid').all()
         for user in users: 
             orders = Order.query.filter_by(user_id=user.id).order_by(Order.placed_on.desc()).all()
-            is_not_completed = False
+            is_not_completed, has_order = False, False
             for order in orders: 
-                if order.placed_on.date() == user.next_delivery_date and not order.is_completed: 
-                    is_not_completed = True
-                    break
+                if order.placed_on.date() == user.next_delivery_date: 
+                    has_order = True
+                    if not order.is_completed: 
+                        is_not_completed = True
+                        break
             next_delivery_date = user.next_delivery_date
-            if not len(orders) or is_not_completed: 
+            if user.first_name == 'Anuradha': 
+                print(len(orders), is_not_completed)
+            if not len(orders) or is_not_completed or (not has_order and user.deliverer_id):  
                 print(f'Shifted Delivery Date Of {user.first_name} {user.last_name}')
                 user.next_delivery_date = date.today()
                 for order in orders: 
@@ -80,7 +84,7 @@ def shift_deliveries():
 def initialize_delivery_date(): 
     with app.app_context(): 
         print('Initializing delivery date')
-        users = User.query.filter_by(next_delivery_date=None, payment_status='Paid').all()
+        users = User.query.filter_by(next_delivery_date=None, payment_status='Paid', plan_pause_date=None).all()
         for user in users: 
             user.next_delivery_date = date.today() + timedelta(days=3)
             print(f'Initialized Delivery Date Of {user.first_name} {user.last_name}')
