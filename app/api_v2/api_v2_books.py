f4fa25a872c43d08e2859b5a2b864eae1bfe9833
LakshayBhandari <LakshayBhandari867@gmail.com>
2024-01-08 01:08:33 +0530

    random offset added

--- app/api_v2/api_v2_books.py
+++ app/api_v2/api_v2_books.py
@@ -24,7 +24,9 @@ randomoffset=5
 
 @api_v2_books.route('/get-book-set')
 def get_book_set(): 
+    global randomoffset
     age = request.args.get('age')
+
     section_name = request.args.get('section_name')
     start = request.args.get('start')
     end = request.args.get('end')
@@ -48,9 +50,21 @@ def get_book_set():
         NewCategory.min_age <= age,
         NewCategory.max_age >= age
     ).order_by(NewCategory.category_order)
+    
+    if start==0:
+     best_seller_category = NewCategory.query.filter_by(name='Best Seller - Most Popular').first()
     if start is not None and end is not None: 
-        categories_query = categories_query.limit(end - start).offset(start)
+        categories_query = categories_query.limit(end - start).offset(start+randomoffset)
+        if randomoffset > 50:
+            randomoffset = 0
+        
+        
+        
+        
     categories = categories_query.all()
+    if start==0:
+     categories.append(best_seller_category)
+   
     book_set = []
     if len(categories) > 1: 
         shuffled_categories = categories[1:]
@@ -65,6 +79,12 @@ def get_book_set():
             NewBook.max_age >= age
         ).all()
         books = [book[0].to_json() for book in books]
+        for book in books:
+         try:
+            book['review_count'] = int(book['review_count'].replace(',', ''))
+         except ValueError:
+            book['review_count'] = 0 
+       
         if category.name == 'Best Seller - Most Popular': 
             random.shuffle(books)
         else: 
@@ -675,11 +695,8 @@ def update_book_quantity():
         new_book.stock_available = stock_available
         new_book.rentals = rentals
         
-        print(new_book.stock_available)
-        print(new_book.rentals)
         db.session.commit()
-        print(new_book.stock_available)
-        print(new_book.rentals)
+        
         return jsonify({"success": True, "book": new_book.to_json()})
    
     book.stock_available = stock_available
