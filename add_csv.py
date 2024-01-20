8fbc7642e71d4c14bc83d682da033d7fd4874fd3
SacCrypt <sachinfulsunge1@gmail.com>
2024-01-20 20:56:08 +0530

    made changes for existing books

--- add_csv.py
+++ add_csv.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from app.models.new_books import NewBook, Book, NewCategory, NewCategoryBook, NewBookSection, Book, NewBookImage
 import openpyxl 
+from sqlalchemy import update
 
 app = create_app()
 db.init_app(app)
@@ -31,17 +32,26 @@ with app.app_context():
                 if isinstance(row[cell].value, float):
                     row[cell].value = int(row[cell].value)
                 temp[header_row[cell]] = row[cell].value
-        if temp and "available" in temp and temp['available'].strip() == "New":
-            temp['isbn'] = str(temp['isbn'])
-            books.append(temp['isbn'])
-            Book.create(temp['name'], temp['image'], temp['isbn'], temp['rating'], temp['review_count'], "", "", None, temp['book description'], 0, None, None, None, None)
-            new_book_instance = NewBook.create(temp['name'], temp['image'], temp['isbn'], temp['rating'], temp['review_count'], temp['min_age'], temp['max_age'],None, None, temp['genres'], int(temp['pages'].split(" ")[0]), None, temp['book description'], temp['publication'], None, None, None, None, ",".join([x for x in [temp['author 1'], temp['author 2'], temp['author 3'], temp['author 4']] if x]))
-            category_id = NewCategory.create(temp['category'], 0, temp['min_age'], temp['max_age'])
-            NewCategoryBook.create(category_id, new_book_instance, 1)
-            for side_image in [temp['side image 1'], temp['side image 2'], temp['side image 3'], temp['side image 4']]:
-                if side_image:
-                    NewBookImage.create(side_image, new_book_instance, 0)
-            print(temp['isbn'])
-    print(len(books), books)
+        if temp:
+            # Existing books logic
+            isbn = str(temp['isbn'])
+            fetch_book = NewBook.query.filter_by(isbn=isbn).first()
+            print("available" in temp)
+            if fetch_book and "available" not in temp:
+                update_new_book = update(NewBook).where(NewBook.isbn == isbn).values(name=temp['name'], image=temp['image'], isbn = temp['isbn'], rating=temp['rating'], review_count=temp['review_count'], min_age=temp['min_age'], max_age=temp['max_age'], pages=int(temp['pages'].split(" ")[0]), dimensions=temp['size'], publisher=temp['publication'], authors=",".join([x for x in [temp['author 1'], temp['author 2'], temp['author 3'], temp['author 4']] if x]), description=temp['book description']).returning(NewBook.id)
+                updated_id = db.engine.execute(update_new_book).fetchone()
+                update_book = update(Book).where(Book.isbn == isbn).values(name=temp['name'], image=temp['image'], isbn=temp['isbn'], rating=temp['rating'], review_count=temp['review_count'], description=temp['book description'])
+                db.engine.execute(update_book)
+                print("UPDATED", isbn)
+            # New books that were to be added 
+            # temp['isbn'] = str(temp['isbn'])
+            # books.append(temp['isbn'])
+            # Book.create(temp['name'], temp['image'], temp['isbn'], temp['rating'], temp['review_count'], "", "", None, temp['book description'], 0, None, None, None, None)
+            # new_book_instance = NewBook.create(temp['name'], temp['image'], temp['isbn'], temp['rating'], temp['review_count'], temp['min_age'], temp['max_age'],None, None, temp['genres'], int(temp['pages'].split(" ")[0]), None, temp['book description'], temp['publication'], None, None, None, None, ",".join([x for x in [temp['author 1'], temp['author 2'], temp['author 3'], temp['author 4']] if x]))
+            # category_id = NewCategory.create(temp['category'], 0, temp['min_age'], temp['max_age'])
+            # NewCategoryBook.create(category_id, new_book_instance, 1)
+            # for side_image in [temp['side image 1'], temp['side image 2'], temp['side image 3'], temp['side image 4']]:
+            #     if side_image:
+            #         NewBookImage.create(side_image, new_book_instance, 0)
             
 
